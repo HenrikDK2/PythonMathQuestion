60ee9ada9cd09d7675aeb23da7605bc3ccbfec13
HenrikDK2 <henrik265@outlook.dk>
2020-11-28 23:13:09 +0100

    Update

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 from colors import colors
 from mathQuestion import mathQuestion
 from openpyxl import Workbook
-from math import floor
 from random import randint
 
+gameResults = []
 score = 0
-wb = Workbook()
-ws = wb.active
-ws.append(["Question", "Answer", "Solution"])
+
+
+def saveData(rows):
+    wb = Workbook()
+    ws = wb.active
+    ws.append(["Question", "Answer", "Solution"])
+    [ws.append(x) for x in rows]
+    wb.save("data.xlsx")
+    wb.close()
 
 
 while True:
     result = mathQuestion()
-    resultArr = [result["question"], result["answer"], result["solution"]]
+    gameResults.append(
+        [result["question"], result["answer"], result["solution"]])
 
     if(result["answer"] == result["solution"]):
         score += 1
-        ws.append(resultArr)
         print(f"{colors.green}Correct{colors.default}, your current score: {colors.yellow+str(score)+colors.default}")
     else:
         print(colors.red+"Game Over"+colors.default)
-        ws.append(resultArr)
-        wb.save("data.xlsx")
-        wb.close()
         break
+
+saveData(gameResults)
